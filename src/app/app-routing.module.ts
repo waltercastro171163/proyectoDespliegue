07214298bc8041ce4306f7c7ef5bb0ec21f8d072
waltercastro171163 <walterc.100@gmail.com>
2022-05-02 19:33:49 -0500

    nuevoDespliegue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { XboxComponent } from './componente/xbox/xbox.component';
+import { InicioComponent } from './componente/inicio/inicio.component';
+import { Ps5Component } from './componente/ps5/ps5.component';
+import { NintentoComponent} from './componente/nintento/nintento.component';
+
+
+const routes: Routes = [
+
+  {path:'xbox',component:XboxComponent},
+  {path:'inicio',component:InicioComponent},
+  {path:'Ps5',component: Ps5Component},
+  {path:'Nintendo',component:NintentoComponent},
+  {path: '', component: InicioComponent }, //*predeterminado al ingresar a la pagina ppal
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BarraComponent } from './componente/barra/barra.component';
+import { InicioComponent } from './componente/inicio/inicio.component';
+import { XboxComponent } from './componente/xbox/xbox.component';
+import { Ps5Component } from './componente/ps5/ps5.component';
+import { NintentoComponent } from './componente/nintento/nintento.component';
+import { FooterComponent } from './componente/footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraComponent,
+    InicioComponent,
+    XboxComponent,
+    Ps5Component,
+    NintentoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
